39b1564a9c84d24ec3113a259c7bb56b4ed5d75d
David Rappo <david.rappo@gmail.com>
2022-06-04 17:48:48 +1000

    longest_substring_without_repeating_characters - Adding unit tests.

--- rust/longest_substring_without_repeating_characters/src/lib.rs
+++ rust/longest_substring_without_repeating_characters/src/lib.rs
@@ -2,9 +2,16 @@ mod solution;
 
 #[cfg(test)]
 mod tests {
+    use crate::solution;
+
+    const ARGUMENTS: [&str; 3] = ["abcabcbb", "bbbbb", "pwwkew"];
+    const EXPECTED_RESULTS: [usize; 3] = [3, 1, 3];
+
     #[test]
-    fn it_works() {
-        let result = 2 + 2;
-        assert_eq!(result, 4);
+    fn all_tests() {
+        for index in 0..ARGUMENTS.len() {
+            let result = solution::length_of_longest_substring(ARGUMENTS[index]);
+            assert_eq!(result, EXPECTED_RESULTS[index]);
+        }
     }
 }
