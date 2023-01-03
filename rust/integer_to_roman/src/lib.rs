2b37e1e2a5289d7a62c705d7ffe41bdc5a539651
David Rappo <david.rappo@gmail.com>
2023-01-03 13:50:35 +1100

    Integer To Roman - Adding additional unit tests.

--- rust/integer_to_roman/src/lib.rs
+++ rust/integer_to_roman/src/lib.rs
@@ -4,12 +4,13 @@ mod solution;
 mod tests {
     use crate::solution::int_to_roman;
 
-    const PARAMETER_1: [i32; 17] = [
-        3, 58, 1994, 4, 9, 40, 90, 400, 900, 5, 10, 50, 100, 500, 1000, 104, 1009,
+    const PARAMETER_1: [i32; 22] = [
+        3, 58, 1994, 4, 9, 40, 90, 400, 900, 5, 10, 50, 100, 500, 1000, 104, 1009, 550, 540, 390,
+        444, 999,
     ];
-    const EXPECTED_RESULT: [&str; 17] = [
+    const EXPECTED_RESULT: [&str; 22] = [
         "III", "LVIII", "MCMXCIV", "IV", "IX", "XL", "XC", "CD", "CM", "V", "X", "L", "C", "D",
-        "M", "CIV", "MIX",
+        "M", "CIV", "MIX", "DL", "DXL", "CCCXC", "CDXLIV", "CMXCIX",
     ];
 
     #[test]
