49cc22e18aaf75e6f8e302a541dc72d941184400
David Rappo <david.rappo@gmail.com>
2023-04-25 16:30:40 +1000

    four_sum - Continuing.

--- rust/four_sum/src/lib.rs
+++ rust/four_sum/src/lib.rs
@@ -2,39 +2,35 @@ mod solution;
 
 #[cfg(test)]
 mod tests {
-    use std::collections::HashSet;
-
     use crate::solution;
 
-    fn create_hash_set(slice: &[i32]) -> HashSet<i32> {
-        let mut hash_set = HashSet::new();
-        for x in slice {
-            hash_set.insert(*x);
-        }
-
-        hash_set
+    fn create_sorted_vec(slice: &[i32]) -> Vec<i32> {
+        let mut v = vec![];
+        v.reserve(slice.len());
+        v.extend(slice);
+        v.sort();
+        v
     }
 
+    // TODO: Check this.
     fn check(result: &[Vec<i32>], expected_result: &[Vec<i32>]) -> bool {
         if result.len() != expected_result.len() {
             return false;
         }
 
-        for (index, result_vec) in result.iter().enumerate() {
-            let result_hash_set = create_hash_set(result_vec);
-            let expected_result_slice = &expected_result[index];
-            let expected_result_hash_set = create_hash_set(expected_result_slice);
-            let is_result_subset = result_hash_set.is_subset(&expected_result_hash_set);
-            if result_hash_set.len() != expected_result_hash_set.len() {
-                return false;
-            }
-
-            if !is_result_subset {
-                return false;
+        let mut count = 0;
+        for result_vec in result.iter() {
+            for expected_result_vec in expected_result.iter() {
+                let sorted_result_vec = create_sorted_vec(result_vec);
+                let sorted_expected_result_vec = create_sorted_vec(expected_result_vec);
+                if sorted_result_vec == sorted_expected_result_vec {
+                    count += 1;
+                    break;
+                }
             }
         }
 
-        true
+        count == expected_result.len()
     }
 
     #[test]
@@ -43,8 +39,6 @@ mod tests {
         let target = 0;
         let expected_result = vec![vec![-2, -1, 1, 2], vec![-2, 0, 0, 2], vec![-1, 0, 0, 1]];
         let result = solution::four_sum(numbers, target);
-        // TODO:
-        println!("{:?}", result);
         let check_result = check(&result, &expected_result);
         assert!(check_result);
     }
